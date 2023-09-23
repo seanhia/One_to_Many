8111a82ad356d76be746ba50dd6a98e46084bd06
seanhia <seanhia15@gmail.com>
2023-09-22 19:23:37 -0700

    added more to Department.py

--- Department.py
+++ Department.py
@@ -2,6 +2,8 @@ from orm_base import Base
 from IntrospectionFactory import IntrospectionFactory
 from db_connection import engine
 from sqlalchemy import Table
+from sqlalchemy import Column, Integer, UniqueConstraint, Identity
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship, column_property
 from typing import List  # Use this for the list of courses offered by the department
 from constants import START_OVER, INTROSPECT_TABLES, REUSE_NO_INTROSPECTION
@@ -27,8 +29,7 @@ if introspection_type == START_OVER or introspection_type == REUSE_NO_INTROSPECT
 
         Note, this is just a shell of the Department class.  There are additional
         columns needed, but this is enough to demonstrate one-to-many relationships."""
-        __tablename__ = DC.__tablename__
-        abbreviation: Mapped[str] = DC.abbreviation
+
         """This is a bi-directional relationship.  The Department class manages
         a list of Courses, and the Course class manages an OO reference to the
         "owning" Department.
@@ -37,20 +38,42 @@ if introspection_type == START_OVER or introspection_type == REUSE_NO_INTROSPECT
         imports Departments.py.  If I try to import Course.py here, I'll set up
         a cyclic import loop and Python will not be able to interpret either of
         those class definition files."""
+        __tablename__ = DC.__tablename__
+        """abbreviation: Mapped[str] = DC.abbreviation
         courses: Mapped[List["Course"]] = DC.courses
-        name: Mapped[str] = DC.name
+        name: Mapped[str] = DC.name"""
+        name: Mapped[str] = mapped_column('name', String(50), nullable=False, primary_key=True)
+        abbreviation: Mapped[str] = mapped_column('abbreviation', String(6), nullable=False)
+        chairName: Mapped[str] = mapped_column('chair_name', String(80), nullable=False)
+        building: Mapped[str] = mapped_column('building', String(10), nullable=False)
+        office: Mapped[int] = mapped_column('office', Integer, nullable=False)
+        description: Mapped[str] = mapped_column('description', String(80), nullable=False)
+
         # __table_args__ can best be viewed as directives that we ask SQLAlchemy to
         # send to the database.  In this case, that we want two separate uniqueness
         # constraints (candidate keys).
-        __table_args__ = DC.__table_args__
+        __table_args__ = (UniqueConstraint("abbreviation", name="departments_uk_01"),
+                          UniqueConstraint("chair_name", name="departments_uk_02"),
+                          UniqueConstraint("building", "office", name="departments_uk_03"),
+                          UniqueConstraint("description", name="departments_uk_04"))
 
         """The __init__ function appears to be special in SQLAlchemy.  I'm unable to 
         leave that out when the class is initially declared, and then add it in afterwards.
         So I'm defining the exact same __init__ method both for the start over as well
         as the introspection case just to get past this interesting issue and move on."""
-        def __init__(self, abbreviation: str, name: str):
-            self.abbreviation = abbreviation
+
+        def __init__(self, name: str, abbreviation: str, chair_name: str, building: str, office: int, description: str):
             self.name = name
+            self.abbreviation = abbreviation
+            self.chairName = chair_name
+            self.building = building
+            self.office = office
+            self.description = description
+
+        def __str__(self):
+            return f"Name: {self.name} \nAbbreviation: {self.abbreviation}\nChair Name: {self.chairName}\nBuilding: {self.building} Office: {self.office}\nDescription: {self.description}\n"
+
+
 elif introspection_type == INTROSPECT_TABLES:
     # We need to connect to the database to introspect the table.  So I'm getting that done
     # now, rather than in main.  Connection is a singleton factory of sorts.
@@ -64,10 +87,12 @@ elif introspection_type == INTROSPECT_TABLES:
 
         # The courses list will not get created just from introspecting the database, so I'm doing that here.
         courses: Mapped[List["Course"]] = DC.courses
+        abbreviation: Mapped[str] = column_property(__table__.c.abbreviation)
+
         # I'm not actually overriding the attribute name here, I just want to see if I can do it.
         # The __table__ attribute refers to the Table object that we created by introspection.
         # More on metadata: https://docs.sqlalchemy.org/en/20/core/metadata.html
-        abbreviation: Mapped[str] = column_property(__table__.c.abbreviation)
+
 
         def __init__(self, abbreviation: str, name: str):
             self.abbreviation = abbreviation
