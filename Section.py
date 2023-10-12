5cd2890e2ce9b30ed87b3b1653d23fe083100794
lucas <lucas.gonzalez@student.csulb.edu>
2023-10-12 16:34:14 -0700

    updated section

--- Section.py
+++ Section.py
@@ -3,7 +3,7 @@ from sqlalchemy import Table
 from sqlalchemy.orm import Mapped, mapped_column, relationship, column_property
 from sqlalchemy import String, Integer
 from sqlalchemy.types import Time
-from sqlalchemy import UniqueConstraint, ForeignKey, ForeignKeyConstraint, CheckConstraint
+from sqlalchemy import UniqueConstraint, ForeignKeyConstraint, CheckConstraint
 from IntrospectionFactory import IntrospectionFactory
 from Department import Department
 from Course import Course
@@ -19,33 +19,37 @@ if introspection_type == START_OVER or REUSE_NO_INTROSPECTION:
          at 3:30 whereas Section 2 meets at 12 noon."""
         __tablename__ = "sections"
 
-        departmentAbbreviation: Mapped[str] = mapped_column('department_abbreviation', String(10), ForeignKey("departments.abbreviation"), primary_key=True)
-        courseNumber: Mapped[int] = mapped_column('course_number', Integer, ForeignKey("course.course_number"), primary_key=True)
+        departmentAbbreviation: Mapped[str] = mapped_column('department_abbreviation', String(10), primary_key=True)
+        courseNumber: Mapped[int] = mapped_column('course_number', Integer, primary_key=True)
         sectionNumber: Mapped[int] = mapped_column('section_number', Integer, primary_key=True)
-        semester: Mapped[str] = mapped_column('semester', String(10), CheckConstraint("semester IN('Fall','Spring','Winter','Summer I','Summer II')", name="semester_values_check"), nullable=False, primary_key=True)  # cuz mandatory
+        semester: Mapped[str] = mapped_column('semester', String(10), CheckConstraint("semester IN('Fall','Spring','Winter','Summer I','Summer II')",
+                                                                                      name="semester_values_check"), nullable=False, primary_key=True)  # cuz mandatory
         sectionYear: Mapped[int] = mapped_column('section_year', Integer, nullable=False,
                                                   primary_key=True)
-        building: Mapped[str] = mapped_column('building', String(6),CheckConstraint("building IN('VEC','ECS','EN2','EN3','EN4','ET','SSPA')", name="building_values_check") , nullable=False)
+        building: Mapped[str] = mapped_column('building', String(6), CheckConstraint("building IN('VEC','ECS','EN2','EN3','EN4','ET','SSPA')",
+                                                                                    name="building_values_check"), nullable=False)
         room: Mapped[int] = mapped_column('room', Integer, nullable=False)
-        schedule: Mapped[str] = mapped_column('schedule', String(6), CheckConstraint("schedule IN('MW','TuTh','MWF','F','S')", name="schedule_values_check") ,nullable=False)
+        schedule: Mapped[str] = mapped_column('schedule', String(6), CheckConstraint("schedule IN('MW','TuTh','MWF','F','S')",
+                                                                                     name="schedule_values_check"), nullable=False)
         startTime: Mapped[Time] = mapped_column('start_time', Time, nullable=False)
         instructor: Mapped[str] = mapped_column('instructor', String(80), nullable=False)
 
-        courses: Mapped["Course"] = relationship(back_populates="section")
+        course: Mapped["Course"] = relationship(back_populates="sections")
 
         __table_args__ = (UniqueConstraint("section_year", "semester", "schedule", "start_time",
                                            "building", "room", name="sections_uk_01"),
                           UniqueConstraint("section_year", "semester", "schedule", "start_time",
-                                           "instructor", name="sections_uk_02"))
-                          #ForeignKeyConstraint([departmentAbbreviation, courseNumber],
-                                               #[Course.department_abbreviation, Course.course_number]))
+                                           "instructor", name="sections_uk_02"),
+                          ForeignKeyConstraint([departmentAbbreviation, courseNumber],
+                                               [Course.department_abbreviation, Course.course_number]))
 
-        def __init__(self, department_abbreviation: str, course_number: int, section_number: int,
+        def __init__(self, course: Course, section_number: int,
                      semester: str, section_year: int, building: str, room: int, schedule: str,
-                     start_time: Time, instructor: str, courses: Course):
+                     start_time: Time, instructor: str):
             # not sure how to use compound primary keys/relationship
-            self.departmentAbbreviation = department_abbreviation
-            self.courseNumber = course_number
+            #self.departmentAbbreviation = department_abbreviation
+            #self.courseNumber = course_number
+            self.set_course(course)
             self.sectionNumber = section_number
             self.semester = semester
             self.sectionYear = section_year
@@ -54,7 +58,6 @@ if introspection_type == START_OVER or REUSE_NO_INTROSPECTION:
             self.schedule = schedule
             self.startTime = start_time
             self.instructor = instructor
-            # self.set_course(courses) not sure if i need this here or not, and in __init__ ^
 
 elif introspection_type == INTROSPECT_TABLES:
     class Section(Base):
@@ -62,20 +65,19 @@ elif introspection_type == INTROSPECT_TABLES:
         departmentAbbreviation: Mapped[str] = column_property(__table__.c.department_abbreviation)
         courseNumber: Mapped[str] = column_property(__table__.c.course_number)
         sectionNumber: Mapped[str] = column_property(__table__.c.section_number)
-        semester: Mapped[str] = column_property(__table__.c.semester)
-        sectionYear: Mapped[str] = column_property(__table__.c.section_year)
-        building: Mapped[str] = column_property(__table__.c.building)
-        room: Mapped[str] = column_property(__table__.c.room)
-        startTime: Mapped[str] = column_property(__table__.c.start_time)
-        schedule: Mapped[str] = column_property(__table__.c.schedule)
-        instructor: Mapped[str] = column_property(__table__.c.instructor)
 
-        def __init__(self, department_abbreviation: str, course_number: int, section_number: int,
-                     semester: str, section_year: int, building: str, room: int, schedule: str,
-                     start_time: Time, instructor: str, courses: Course):
+        sectionYear: Mapped[str] = column_property(__table__.c.section_year)
+
+        startTime: Mapped[str] = column_property(__table__.c.start_time)
+
+
+
+        def __init__(self, course: Course, section_number: int, semester: str, section_year: int, building: str,
+                     room: int, schedule: str, start_time: Time, instructor: str):
             # not sure how to use compound primary keys/relationship
-            self.departmentAbbreviation = department_abbreviation
-            self.courseNumber = course_number
+            #self.departmentAbbreviation = department_abbreviation
+            #self.courseNumber = course_number
+            self.set_course(course)
             self.sectionNumber = section_number
             self.semester = semester
             self.sectionYear = section_year
@@ -86,12 +88,17 @@ elif introspection_type == INTROSPECT_TABLES:
             self.instructor = instructor
 
 #indenting might help initialize the selfs
-    def __str__(self):
-        return f"Department Abbreviation: {self.departmentAbbreviation}\nCourse Number: {self.courseNumber}\nSection Number: {self.sectionNumber}\nSemester: {self.semester}\nSection Year: {self.sectionYear}\nBuilding: {self.building}\nRoom: {self.room}\nSchedule: {self.schedule}\nStart Time: {self.startTime}\nInstructor: {self.instructor}"
+def set_course(self, course: Course):
+    self.course = course
+    self.courseNumber = course.courseNumber
+def __str__(self):
+    return f"Department Abbreviation: {self.departmentAbbreviation}\nCourse Number: {self.courseNumber}\n" \ 
+           f"Section Number: {self.sectionNumber}\nSemester: {self.semester}\nSection Year: {self.sectionYear}\n" \
+           f"Building: {self.building}\nRoom: {self.room}\nSchedule: {self.schedule}\nStart Time: {self.startTime}\n" \
+           f"Instructor: {self.instructor}"
 
-    def set_course(self, course: Course):
-        self.course = course
-        self.courseNumber = course.courseNumber
 
-    setattr(Section, '__str__', __str__)
-    setattr(Section, 'set_course', set_course)
+
+
+setattr(Section, 'set_course', set_course)
+setattr(Section, '__str__', __str__)
